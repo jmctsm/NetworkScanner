cff941b40b52140f0fe49f964d6f16cbe9303478
jmctsm <james.copeland@gmail.com>
2021-03-02 18:40:29 -0600

    Add multiprocess ping and tests for it.  Changed scanner to use it too

---
+++ scan_mods/mp_pinger.py
@@ -0,0 +1,128 @@
+#!python
+
+import ipaddress
+from pythonping import ping
+import re
+import multiprocessing
+import time
+
+
+def __ping_address(address):
+    """
+    This will take an IP address in the IP objects and ping it.
+    It will return either a string that says it timed out or it
+    return a tuple of the address and response times
+
+    This is pseudo private and should be called from the main pinger function.
+
+    Tried to optomize so it can be run in a multi-processor environment
+
+    Args:
+        address (ipaddress.IPv4Address) : ipaddress in ipaddress.ipv4address class type
+
+    Return:
+        tuple/str : tuple of ip address and response times if the system is up or a TIMEOUT string if not
+    """
+    if not isinstance(address, ipaddress.IPv4Address):
+        raise ValueError(f"{address} since it is not an IPv4Address")
+    print(f"Pinging {address}", end=" (")
+    result = ping(str(address), timeout=1, count=3)
+    timeout_pattern = "Round Trip Times min/avg/max is 1000/1000.0/1000 ms"
+    timeout_result = re.search(timeout_pattern, str(result), re.IGNORECASE)
+    active_result = False
+    if timeout_result:
+        print("Not Responding)")
+        return "TIMEOUT"
+    else:
+        active_pattern = "Round Trip Times min\/avg\/max is (.*)\/(.*)\/(.*) ms"
+        active_result = re.search(active_pattern, str(result), re.IGNORECASE)
+    if active_result:
+        up_result = (
+            float(active_result.group(1)),
+            float(active_result.group(2)),
+            float(active_result.group(3)),
+        )
+        print(f"{up_result[0]} ms, {up_result[1]} ms, {up_result[2]} ms)")
+        return (address, up_result)
+
+
+def pinger(addresses):
+    """
+    This will take a list of IP addresses in the IP objects and ping them.
+    It will return a dictionary of addresses that are reachable along with
+    the response times for each address.
+
+    Args:
+        addresses (list) : list of IP address objects to ping
+
+    Return:
+        list : list of IP address objects that are reachable and the
+            response time of each one
+    """
+    # raise an error is an empty list is passed to the function
+    if len(addresses) == 0:
+        raise ValueError(
+            "Looks like the network didn't work for getting IPs.  Bye Bye!!"
+        )
+    if not isinstance(addresses, list):
+        raise TypeError(
+            "Looks like a list was not passed to pinger.  Please try again."
+        )
+    for address in addresses:
+        if not isinstance(address, ipaddress.IPv4Address):
+            raise ValueError(f"{address} since it is not an IPv4Address")
+    with multiprocessing.Pool() as pool:
+        ping_results = pool.map(__ping_address, addresses)
+
+    active_dict = {}
+    if len(ping_results) == 0:
+        raise ValueError("Nothing was alive.  Pick a subnet that has something alive")
+
+    for item in ping_results:
+        if item is None:
+            raise ValueError("The return value was not correct.")
+        if isinstance(item, str) and item == "TIMEOUT":
+            continue
+        if isinstance(item[1], tuple) and len(item[1]) == 3:
+            active_dict[item[0]] = {"ping": item[1]}
+        else:
+            print(item)
+            raise ValueError("The return value was not correct.")
+
+    if len(active_dict) > 0:
+        return active_dict
+    else:
+        raise Exception("Nothing was alive.  Pick a subnet that has something alive")
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    # test this out with a home network
+    test_addresses = [
+        ipaddress.ip_network("192.168.1.64/29"),
+        ipaddress.ip_address("192.168.1.65"),
+        ipaddress.ip_network("192.168.89.0/24"),
+    ]
+    # host list to pass to the pinger function
+    hosts_lists = []
+    # iterate through all of the hosts within the address list and append that to the list of
+    # hosts to ping
+    for address in test_addresses:
+        if isinstance(address, ipaddress.IPv4Network):
+            for x in address.hosts():
+                hosts_lists.append(x)
+        elif isinstance(address, ipaddress.IPv4Address):
+            hosts_lists.append(address)
+    # if the length of the hosts_lists is 0, that means there are no IPs to scan and
+    # something went wrong
+    # else start the pinger
+    if len(hosts_lists) == 0:
+        raise ValueError(
+            "Looks like the network didn't work for getting IPs.  Bye Bye!!"
+        )
+    else:
+        active_hosts = pinger(hosts_lists)
+    if len(active_hosts) > 0:
+        print(active_hosts)
+    duration = time.time() - start_time
+    print(f"Total time was {duration} seconds")
---
+++ tests/test_mp_pinger.py
@@ -0,0 +1,150 @@
+import re
+import unittest
+import ipaddress
+import os
+import sys
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+from scan_mods.mp_pinger import pinger
+from scan_mods.mp_pinger import __ping_address as ping_address
+
+
+class TestPinger(unittest.TestCase):
+    """
+    Tests that pinger works
+    """
+
+    test_addresses = [
+        ipaddress.ip_network("192.168.1.64/29"),
+        ipaddress.ip_network("10.0.1.0/29"),
+        ipaddress.ip_network("192.168.89.0/24"),
+        ipaddress.ip_address("192.168.1.65"),
+    ]
+    test_networks = [
+        ipaddress.ip_network("192.168.1.64/29"),
+        ipaddress.ip_network("10.0.1.0/29"),
+        ipaddress.ip_network("10.0.1.0/29"),
+        ipaddress.ip_network("192.168.89.0/24"),
+    ]
+
+    def test_00_all_pass_pinger(self):
+        print("\nStart testing that all pass")
+        for address in self.test_addresses:
+            hosts_lists = []
+            if isinstance(address, ipaddress.IPv4Network):
+                for x in address.hosts():
+                    hosts_lists.append(x)
+            elif isinstance(address, ipaddress.IPv4Address):
+                hosts_lists.append(address)
+            active_hosts = pinger(hosts_lists)
+            self.assertGreaterEqual(len(active_hosts), 0)
+            self.assertIsInstance(active_hosts, dict)
+            for key in active_hosts.keys():
+                self.assertIsInstance(active_hosts[key], dict)
+                self.assertIsInstance(active_hosts[key]["ping"], tuple)
+                self.assertEqual(len(active_hosts[key]["ping"]), 3)
+            print(f"Tests passed for network/address {str(address)}")
+        print("Finish testing that all pass\n")
+
+    def test_01_all_pass_ping_address(self):
+        print("\nStart testing that all pass")
+        for address in self.test_addresses:
+            hosts_lists = []
+            if isinstance(address, ipaddress.IPv4Network):
+                for x in address.hosts():
+                    hosts_lists.append(x)
+            elif isinstance(address, ipaddress.IPv4Address):
+                hosts_lists.append(address)
+            for host in hosts_lists:
+                return_value = ping_address(host)
+                self.assertTrue(
+                    isinstance(return_value, tuple) or isinstance(return_value, str)
+                )
+                if isinstance(return_value, str):
+                    self.assertEqual(return_value, "TIMEOUT")
+                if isinstance(return_value, tuple):
+                    self.assertIsInstance(return_value[0], ipaddress.IPv4Address)
+                    self.assertEqual(return_value[0], host)
+                    self.assertIsInstance(return_value[1], tuple)
+                    self.assertEqual(len(return_value[1]), 3)
+                    self.assertIsInstance(return_value[1][0], float)
+                    self.assertIsInstance(return_value[1][1], float)
+                    self.assertIsInstance(return_value[1][2], float)
+            print(f"Tests passed for network/address {str(address)}")
+        print("Finish testing that all pass\n")
+
+    def test_02_pinger_fail_as_expected(self):
+        print("\nStart testing that all fail due to empty list passed")
+        test_list = []
+        with self.assertRaises(ValueError):
+            pinger(test_list)
+        print("Finish testing that all fail due to empty list passed\n")
+        print("\nStart testing that all fail due to not a list passed")
+        for address in self.test_networks:
+            hosts_lists = []
+            for x in address.hosts():
+                hosts_lists.append(x)
+            with self.assertRaises(TypeError):
+                pinger(tuple(hosts_lists))
+        print("Finish testing that all fail due to not a list passed\n")
+        print("\nStart testing that all fail due to no subnets.  All host bits")
+        with self.assertRaises(ValueError):
+            test_02_addresses = [
+                ipaddress.ip_network("192.168.1.65/29"),
+                ipaddress.ip_network("10.0.1.248/32"),
+            ]
+            for address in test_02_addresses:
+                pinger(address)
+        print("Finish testing that all fail due to no subnets.  All host bits\n")
+
+    def test_03_pass_due_to_no_active_hosts(self):
+        print("\nStart testing that passed due to no active hosts")
+        test_addresses = [
+            ipaddress.ip_network("192.168.1.0/29"),
+            ipaddress.ip_network("10.0.1.16/29"),
+        ]
+        for address in test_addresses:
+            hosts_lists = []
+            for x in address.hosts():
+                hosts_lists.append(x)
+            with self.assertRaises(Exception):
+                pinger(hosts_lists)
+        print("Finish testing that passed due to no active hosts\n")
+
+    def test_04_pass_due_to_no_arguments_passed_to_pinger(self):
+        print("\nStart testing that all fail due to no arguments passed\n")
+        with self.assertRaises(TypeError):
+            pinger()
+        print("\nFinish testing that all fail due to no arguments passed\n")
+
+    def test_05_fail_due_to_no_not_an_IP_to_pinger(self):
+        print("\nStart testing that pinger fails due to an IP not being in the list\n")
+        test_addresses = [
+            ipaddress.ip_address("10.0.1.254"),
+            ipaddress.ip_address("192.168.1.65"),
+            "abc",
+            123,
+        ]
+        with self.assertRaises(ValueError):
+            pinger(test_addresses)
+        print("\nFinish testing that pinger fails due to an IP not being in the list\n")
+
+    def test_06_fail_due_to_no_not_an_IP_to_ping_address(self):
+        print("\nStart testing that pinger fails due to an IP not being in the list\n")
+        test_addresses = [
+            "10.0.1.254",
+            ipaddress.ip_network("10.0.1.16/29"),
+            "abc",
+            123,
+        ]
+        with self.assertRaises(ValueError):
+            for address in test_addresses:
+                ping_address(address)
+        print("\nFinish testing that pinger fails due to an IP not being in the list\n")
+
+
+if __name__ == "__main__":
+    unittest.main()
