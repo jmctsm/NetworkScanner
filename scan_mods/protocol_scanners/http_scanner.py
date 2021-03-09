b127e10982f68600bef4f7c213070d6690d61320
jmctsm <james.copeland@gmail.com>
2021-03-09 03:47:20 -0600

    Fixed so that you can provide a port or defaults to standard ports

--- scan_mods/protocol_scanners/http_scanner.py
+++ scan_mods/protocol_scanners/http_scanner.py
@@ -4,19 +4,19 @@
     Connect to a web server and grab the HTTP headers
 """
 
-from typing import Type
 import requests
 import ipaddress
 import time
 import json
 
 
-def http_scanner(address):
+def http_scanner(address, port=None):
     """
     This will connect to the HTTP server and grab the headers for the HTTP server
 
     Args:
         address (str) : string of the address to connect to (IPv4 format)
+        port (int) : will run the scanner against the port specified.  None given, it uses 80
 
     Return:
         str : headers string formatted
@@ -30,11 +30,27 @@ def http_scanner(address):
         raise ipaddress.AddressValueError(
             f"{address} is not set up to be an IPv4 adddress.  It did not have four octets."
         )
+    if port is not None:
+        if not isinstance(port, int):
+            if isinstance(port, float):
+                raise TypeError(f"{port} is not an int and cannot be made into an int.")
+            try:
+                int(port)
+            except ValueError:
+                raise ValueError(
+                    f"{port} is not an int and cannot be made into an int."
+                )
+            except TypeError:
+                raise TypeError(f"{port} is not an int and cannot be made into an int.")
+        elif int(port) < 0 or int(port) > 65535:
+            raise ValueError(f"Port number has to be between 0 and 65535.")
+    elif port is None:
+        port = 80
     session = requests.Session()
     retry = requests.packages.urllib3.util.retry.Retry(connect=3, backoff_factor=0.5)
     adapter = requests.adapters.HTTPAdapter(max_retries=retry)
     session.mount("http://", adapter)
-    url = f"http://{address}"
+    url = f"http://{address}:{port}"
     return_dict = {}
     while True:
         try:
@@ -63,7 +79,8 @@ if __name__ == "__main__":
     start_time = time.time()
     dict_of_responses = {}
     for address in ["192.168.1.65", "192.168.89.80", "10.0.1.1", "192.168.1.254"]:
-        dict_of_responses[address] = http_scanner(address)
+        for port in [80, 8080, None]:
+            dict_of_responses[f"{address}_{port}"] = http_scanner(address, port)
     print(dict_of_responses)
     print("\n\n\n\n")
     print(json.dumps(dict_of_responses))
--- scan_mods/protocol_scanners/https_scanner.py
+++ scan_mods/protocol_scanners/https_scanner.py
@@ -15,12 +15,13 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-def https_scanner(address):
+def https_scanner(address, port=None):
     """
     This will connect to the HTTPS server and grab the headers for the HTTPS server
 
     Args:
         address (str) : string of the address to connect to (IPv4 format)
+        port (int) : will run the scanner against the port specified.  None given, it uses 443
 
     Return:
         str : headers string formatted
@@ -34,11 +35,27 @@ def https_scanner(address):
         raise ipaddress.AddressValueError(
             f"{address} is not set up to be an IPv4 adddress.  It did not have four octets."
         )
+    if port is not None:
+        if not isinstance(port, int):
+            if isinstance(port, float):
+                raise TypeError(f"{port} is not an int and cannot be made into an int.")
+            try:
+                int(port)
+            except ValueError:
+                raise ValueError(
+                    f"{port} is not an int and cannot be made into an int."
+                )
+            except TypeError:
+                raise TypeError(f"{port} is not an int and cannot be made into an int.")
+        elif int(port) < 0 or int(port) > 65535:
+            raise ValueError(f"Port number has to be between 0 and 65535.")
+    elif port is None:
+        port = 443
     session = requests.Session()
     retry = requests.packages.urllib3.util.retry.Retry(connect=3, backoff_factor=0.5)
     adapter = requests.adapters.HTTPAdapter(max_retries=retry)
     session.mount("https://", adapter)
-    url = f"https://{address}"
+    url = f"https://{address}:{port}"
     return_dict = {}
     while True:
         try:
