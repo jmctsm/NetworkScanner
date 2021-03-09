c3c753dd2cb77ef273fa9d59338fbcfaa5037d6c
jmctsm <james.copeland@gmail.com>
2021-03-09 03:46:55 -0600

    Moved tests to another folder that made sense

--- tests/scan_mod_tests/test_scanner_http_scanner.py
+++ tests/scan_mod_tests/test_scanner_http_scanner.py
@@ -1,5 +1,6 @@
 #!python
 
+from typing import Type
 import unittest
 import os
 import sys
@@ -8,7 +9,8 @@ import json
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
-sys.path.append(parentdir)
+grandparentdir = os.path.dirname(parentdir)
+sys.path.append(grandparentdir)
 
 from scan_mods.protocol_scanners.http_scanner import http_scanner
 
@@ -17,6 +19,8 @@ class TestPortScanner(unittest.TestCase):
     """
     Tests that HTTP port scanner works
     """
+
+    test_good_ports = [80, 8080, None]
 
     def test_00_all_pass_using_http_scanner(self):
         """
@@ -30,12 +34,13 @@ class TestPortScanner(unittest.TestCase):
             "192.168.1.254",
             "192.168.89.1",
         ]:
-            result = http_scanner(address)
-            self.assertIsNotNone(result)
-            self.assertGreaterEqual(len(result), 1)
-            self.assertIsInstance(result, dict)
-            for key in result.keys():
-                self.assertIsInstance(key, str)
+            for port in self.test_good_ports:
+                result = http_scanner(address, port)
+                self.assertIsNotNone(result)
+                self.assertGreaterEqual(len(result), 1)
+                self.assertIsInstance(result, dict)
+                for key in result.keys():
+                    self.assertIsInstance(key, str)
         print("Finished test for all pass with http_scanner\n")
 
     def test_01_pass_non_string_and_fail(self):
@@ -87,6 +92,45 @@ class TestPortScanner(unittest.TestCase):
         self.assertGreaterEqual(len(json_output), 1)
         self.assertIsInstance(json_output, str)
         print("Finished test http_scanner can create valid JSON\n")
+
+    def test_04_pass_bad_port_value_and_fail(self):
+        """
+        Tests that the HTTP port scanner fails when passed bad port value
+        """
+        print("\nStarting test for http_scanner failing when use a bad port value")
+        for address in ["1.1.1", "1", "a"]:
+            with self.assertRaises(ipaddress.AddressValueError):
+                http_scanner(address)
+        for address in [
+            "192.168.1.65",
+            "10.0.1.1",
+            "192.168.89.80",
+            "192.168.1.254",
+            "192.168.89.1",
+        ]:
+
+            for port in [
+                "a",
+            ]:
+                with self.assertRaises(ValueError):
+                    http_scanner(address, port)
+            for port in [
+                1.1,
+                (1, 1),
+                [1, 1],
+            ]:
+                with self.assertRaises(TypeError):
+                    http_scanner(address, port)
+
+            for port in [
+                -1,
+                1_000_000,
+                65536,
+            ]:
+                with self.assertRaises(ValueError):
+                    http_scanner(address, port)
+
+        print("Finished test for http_scanner failing when use a bad port value\n")
 
 
 if __name__ == "__main__":
