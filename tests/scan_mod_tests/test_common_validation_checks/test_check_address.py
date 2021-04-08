9d980b0d96fe04e0b3ee4b5a738d61657c7923f5
jmctsm <james.copeland@gmail.com>
2021-04-08 09:13:44 -0500

    converted it from pytest to unittest

--- tests/scan_mod_tests/test_common_validation_checks/test_check_address.py
+++ tests/scan_mod_tests/test_common_validation_checks/test_check_address.py
@@ -1,5 +1,6 @@
 #! python
 
+import unittest
 import os
 import sys
 
@@ -19,28 +20,28 @@ else:
 from scan_mods.common_validation_checks.check_address import check_address
 
 import ipaddress
-import pytest
 
 
-@pytest.mark.parametrize(
-    "address, expected",
-    [
-        ("192.168.1.1", "192.168.1.1"),
-        (ipaddress.ip_address("192.168.1.1"), "192.168.1.1"),
-    ],
-)
-def test_01_all_pass(address, expected):
-    assert check_address(address) == expected
+class TestCheckAddress(unittest.TestCase):
+    def test_01_all_pass(self):
+        test_dict = {
+            ipaddress.ip_address("192.168.1.1"): "192.168.1.1",
+            "192.168.1.1": "192.168.1.1",
+        }
+        for test_address, return_value in test_dict.items():
+            self.assertEqual(check_address(test_address), return_value)
+
+    def test_02_raise_exceptions(self):
+        test_dict = {
+            "test01": (None, ValueError),
+            "test02": ("abc", ipaddress.AddressValueError),
+            "test03": (1.1, TypeError),
+        }
+        for test_tuple in test_dict.values():
+            test_address, exception_to_raise = test_tuple
+            with self.assertRaises(exception_to_raise):
+                check_address(test_address)
 
 
-@pytest.mark.parametrize(
-    "address, exception_to_raise",
-    [
-        (None, ValueError),
-        ("abc", ipaddress.AddressValueError),
-        (1.1, TypeError),
-    ],
-)
-def test_02_raise_exceptions(address, exception_to_raise):
-    with pytest.raises(exception_to_raise):
-        check_address(address)
+if __name__ == "__main__":
+    unittest.main()
