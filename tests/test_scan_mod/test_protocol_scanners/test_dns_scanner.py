08949e78e7ac6d1ebaeb836ad11dee16bb10a606
jmctsm <james.copeland@gmail.com>
2021-04-09 16:58:16 -0500

    test passes

--- tests/test_scan_mod/test_protocol_scanners/test_dns_scanner.py
+++ tests/test_scan_mod/test_protocol_scanners/test_dns_scanner.py
@@ -6,16 +6,19 @@ import sys
 import ipaddress
 import json
 
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)
-grandparentdir = os.path.dirname(parentdir)
-sys.path.append(grandparentdir)
+if "scan_mods" in os.listdir(os.getcwd()):
+    sys.path.append(os.getcwd())
+
+else:
+    path = "../"
+    while True:
+        if "scan_mods" in os.listdir(path):
+            sys.path.append(path)
+            break
+        else:
+            path += "../"
 
 import scan_mods.protocol_scanners.dns_scanner
-from scan_mods.protocol_scanners.dns_scanner import (
-    __validate_server_domain_name as validate_server_domain_name,
-    __strip_alligators as strip_alligators,
-)
 
 
 class TestPortScanner(unittest.TestCase):
@@ -31,7 +34,68 @@ class TestPortScanner(unittest.TestCase):
     ]
     local_domain_name = ["test.local", "www.google.com", "google.com", "test.test"]
 
-    def test_00_validate_server_domainname_function_all_pass(self):
+    def test_01_strip_alligators_all_pass(self):
+        """
+        This will test the strip alligators function and make sure that it passes
+        """
+        print(
+            "\nStarting the test for the strip_alligators function passes correctly..."
+        )
+        strings_to_test = [
+            "<first test>",
+            "<<second test>>",
+            "<<<third test>>>",
+            "<<<fourth test>",
+            "fifth test",
+            "<sixth test]",
+            "[seventh test>",
+        ]
+        good_values = [
+            "[first test]",
+            "[[second test]]",
+            "[[[third test]]]",
+            "[[[fourth test]",
+            "fifth test",
+            "[sixth test]",
+            "[seventh test]",
+        ]
+        counter = 0
+        while counter < len(strings_to_test):
+            self.assertEqual(
+                scan_mods.protocol_scanners.dns_scanner.strip_alligators(
+                    strings_to_test[counter]
+                ),
+                good_values[counter],
+            )
+            counter += 1
+
+        print(
+            "Finished the test for the strip_alligators function passes correctly...\n"
+        )
+
+    def test_02_strip_alligators_raises_error(self):
+        """
+        This will test the strip alligators function and make sure that it raises an error
+        """
+        print(
+            "\nStarting the test for the strip_alligators function raises an error..."
+        )
+
+        class Fail_Class:
+            def __init__(self):
+                self.name = "name"
+
+            def __str__(self):
+                raise TypeError
+
+        with self.assertRaises(TypeError):
+            scan_mods.protocol_scanners.dns_scanner.strip_alligators(p=Fail_Class())
+
+        print(
+            "Finished the test for the strip_alligators function raises an error...\n"
+        )
+
+    def test_03_validate_server_domainname_function_all_pass(self):
         """
         Tests that the validate function returns correctly when good values are passed in
         """
@@ -40,19 +104,29 @@ class TestPortScanner(unittest.TestCase):
         )
         for server in self.test_servers:
             for domainname in self.local_domain_name:
-                result = validate_server_domain_name(server, domainname)
+                result = (
+                    scan_mods.protocol_scanners.dns_scanner.validate_server_domain_name(
+                        server, domainname
+                    )
+                )
                 self.assertIsInstance(result, tuple)
                 self.assertTupleEqual(result, (server, domainname))
                 self.assertEqual(result[0], server)
                 self.assertEqual(result[1], domainname)
-                result = validate_server_domain_name(
-                    server=None, domain_name=domainname
+                result = (
+                    scan_mods.protocol_scanners.dns_scanner.validate_server_domain_name(
+                        server=None, domain_name=domainname
+                    )
                 )
                 self.assertIsInstance(result, tuple)
                 self.assertTupleEqual(result, ("192.168.89.80", domainname))
                 self.assertEqual(result[0], "192.168.89.80")
                 self.assertEqual(result[1], domainname)
-                result = validate_server_domain_name(server, domain_name=None)
+                result = (
+                    scan_mods.protocol_scanners.dns_scanner.validate_server_domain_name(
+                        server, domain_name=None
+                    )
+                )
                 self.assertIsInstance(result, tuple)
                 self.assertTupleEqual(result, (server, "test.local"))
                 self.assertEqual(result[0], server)
@@ -61,7 +135,7 @@ class TestPortScanner(unittest.TestCase):
             "Finished the test for the validation function when all good values are passed in...\n"
         )
 
-    def test_01_validate_failures(self):
+    def test_04_validate_failures(self):
         """
         This will make sure the validate function fails correctly
         """
@@ -77,15 +151,19 @@ class TestPortScanner(unittest.TestCase):
         ]
         for bad_server_value in bad_server_values:
             with self.assertRaises(ValueError):
-                validate_server_domain_name(server=bad_server_value, domain_name=None)
+                scan_mods.protocol_scanners.dns_scanner.validate_server_domain_name(
+                    server=bad_server_value, domain_name=None
+                )
         for bad_domain_value in bad_domain_values:
             with self.assertRaises(ValueError):
-                validate_server_domain_name(server=None, domain_name=bad_domain_value)
+                scan_mods.protocol_scanners.dns_scanner.validate_server_domain_name(
+                    server=None, domain_name=bad_domain_value
+                )
         print(
             "Finished the test for the validation function fails with bad values are passed in...\n"
         )
 
-    def test_02_udp_dns_scanner_all_pass(self):
+    def test_05_udp_dns_scanner_all_pass(self):
         """
         This will make sure the udp_dns_scanner can pass correctly
         """
@@ -120,7 +198,7 @@ class TestPortScanner(unittest.TestCase):
             "Finished the test for the udp_dns_scanner function passed correctly...\n"
         )
 
-    def test_03_tcp_dns_scanner_all_pass(self):
+    def test_06_tcp_dns_scanner_all_pass(self):
         """
         This will make sure the tcp_dns_scanner can pass correctly
         """
@@ -155,66 +233,7 @@ class TestPortScanner(unittest.TestCase):
             "Finished the test for the tcp_dns_scanner function passed correctly...\n"
         )
 
-    def test_04_strip_alligators_all_pass(self):
-        """
-        This will test the strip alligators function and make sure that it passes
-        """
-        print(
-            "\nStarting the test for the __strip_alligators function passes correctly..."
-        )
-        strings_to_test = [
-            "<first test>",
-            "<<second test>>",
-            "<<<third test>>>",
-            "<<<fourth test>",
-            "fifth test",
-            "<sixth test]",
-            "[seventh test>",
-        ]
-        good_values = [
-            "[first test]",
-            "[[second test]]",
-            "[[[third test]]]",
-            "[[[fourth test]",
-            "fifth test",
-            "[sixth test]",
-            "[seventh test]",
-        ]
-        counter = 0
-        while counter < len(strings_to_test):
-            self.assertEqual(
-                strip_alligators(strings_to_test[counter]),
-                good_values[counter],
-            )
-            counter += 1
-
-        print(
-            "Finished the test for the __strip_alligators function passes correctly...\n"
-        )
-
-    def test_05_strip_alligators_raises_error(self):
-        """
-        This will test the strip alligators function and make sure that it raises an error
-        """
-        print(
-            "\nStarting the test for the __strip_alligators function raises an error..."
-        )
-
-        class Fail_Class:
-            def __init__(self):
-                self.name = "name"
-
-            def __str__(self):
-                raise TypeError
-
-        with self.assertRaises(TypeError):
-            strip_alligators(p=Fail_Class())
-
-        print(
-            "Finished the test for the __strip_alligators function raises an error...\n"
-        )
-
-    def test_06_tcp_udp_can_be_used_in_json(self):
+    def test_07_tcp_udp_can_be_used_in_json(self):
         """
         This will make sure the tcp and udp scanner output can be used in json
         """
