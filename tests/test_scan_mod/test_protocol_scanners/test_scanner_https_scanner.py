de388ab9327d453ee9b2d31e90f4feae6f9f1c83
jmctsm <james.copeland@gmail.com>
2021-04-13 09:37:55 -0500

    HTTPS scanner unit test passed

--- tests/test_scan_mod/test_protocol_scanners/test_scanner_https_scanner.py
+++ tests/test_scan_mod/test_protocol_scanners/test_scanner_https_scanner.py
@@ -1,17 +1,26 @@
 #!python
 
 import unittest
+from unittest.mock import patch
+import requests
 import os
 import sys
 import ipaddress
 import json
 
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)
-grandparentdir = os.path.dirname(parentdir)
-sys.path.append(grandparentdir)
+if "scan_mods" in os.listdir(os.getcwd()):
+    sys.path.append(os.getcwd())
 
-from scan_mods.protocol_scanners.https_scanner import https_scanner
+else:
+    path = "../"
+    while True:
+        if "scan_mods" in os.listdir(path):
+            sys.path.append(path)
+            break
+        else:
+            path += "../"
+
+import scan_mods.protocol_scanners.https_scanner
 
 # Disable the SSL warning
 import urllib3
@@ -24,22 +33,25 @@ class TestPortScanner(unittest.TestCase):
     Tests that HTTPS port scanner works
     """
 
-    test_good_ports = [443, 8443, None]
+    good_ports = [443, 8443, None]
+    good_servers = [
+        "192.168.0.254",
+        "192.168.89.80",
+        "192.168.89.254",
+        "192.168.89.251",
+    ]
 
-    def test_00_all_pass_using_https_scanner(self):
+    def test_01_all_pass_using_https_scanner(self):
         """
         Tests that the HTTPS port scanner passes initial tests
         """
         print("\nStarting test for all pass with https_scanner")
-        for address in [
-            "192.168.1.65",
-            "10.0.1.1",
-            "192.168.89.80",
-            "192.168.1.254",
-            "192.168.89.1",
-        ]:
-            for port in self.test_good_ports:
-                result = https_scanner(address, port)
+        for address in self.good_servers:
+            for port in self.good_ports:
+                print(f"Scanning {address}:{port}")
+                result = scan_mods.protocol_scanners.https_scanner.https_scanner(
+                    address, port
+                )
                 self.assertIsNotNone(result)
                 self.assertGreaterEqual(len(result), 1)
                 self.assertIsInstance(result, dict)
@@ -47,7 +59,7 @@ class TestPortScanner(unittest.TestCase):
                     self.assertIsInstance(key, str)
         print("Finished test for all pass with https_scanner\n")
 
-    def test_01_pass_non_string_and_fail(self):
+    def test_02_pass_non_string_and_fail(self):
         """
         Tests that the HTTPS port scanner fails when passed a non-string value
         """
@@ -60,10 +72,10 @@ class TestPortScanner(unittest.TestCase):
             {"test": "test"},
         ]:
             with self.assertRaises(TypeError):
-                https_scanner(address)
+                scan_mods.protocol_scanners.https_scanner.https_scanner(address)
         print("Finished test for https_scanner failing for using a non-string\n")
 
-    def test_02_pass_non_IPv4able_arg_and_fail(self):
+    def test_03_pass_non_IPv4able_arg_and_fail(self):
         """
         Tests that the HTTPS port scanner fails when passed a non-IPv4-able string
         """
@@ -72,59 +84,33 @@ class TestPortScanner(unittest.TestCase):
         )
         for address in ["1.1.1", "1", "a"]:
             with self.assertRaises(ipaddress.AddressValueError):
-                https_scanner(address)
+                scan_mods.protocol_scanners.https_scanner.https_scanner(address)
         print(
             "Finished test for https_scanner failing for using a non_IP-able address\n"
         )
-
-    def test_03_can_create_valid_json(self):
-        """
-        Tests that the HTTPS port scanner can create valid json
-        """
-        print("\nStarting test https_scanner can create valid JSON")
-        dict_of_results = {}
-        for address in [
-            "192.168.1.65",
-            "10.0.1.1",
-            "192.168.89.80",
-            "192.168.1.254",
-            "192.168.89.1",
-        ]:
-            dict_of_results[address] = https_scanner(address)
-        json_output = json.dumps(dict_of_results)
-        self.assertIsNotNone(json_output)
-        self.assertGreaterEqual(len(json_output), 1)
-        self.assertIsInstance(json_output, str)
-        print("Finished test https_scanner can create valid JSON\n")
 
     def test_04_pass_bad_port_value_and_fail(self):
         """
         Tests that the HTTPS port scanner fails when passed bad port value
         """
         print("\nStarting test for https_scanner failing when use a bad port value")
-        for address in ["1.1.1", "1", "a"]:
-            with self.assertRaises(ipaddress.AddressValueError):
-                https_scanner(address)
-        for address in [
-            "192.168.1.65",
-            "10.0.1.1",
-            "192.168.89.80",
-            "192.168.1.254",
-            "192.168.89.1",
-        ]:
-
+        for address in self.good_servers:
             for port in [
                 "a",
             ]:
                 with self.assertRaises(ValueError):
-                    https_scanner(address, port)
+                    scan_mods.protocol_scanners.https_scanner.https_scanner(
+                        address, port
+                    )
             for port in [
                 1.1,
                 (1, 1),
                 [1, 1],
             ]:
                 with self.assertRaises(TypeError):
-                    https_scanner(address, port)
+                    scan_mods.protocol_scanners.https_scanner.https_scanner(
+                        address, port
+                    )
 
             for port in [
                 -1,
@@ -132,9 +118,54 @@ class TestPortScanner(unittest.TestCase):
                 65536,
             ]:
                 with self.assertRaises(ValueError):
-                    https_scanner(address, port)
+                    scan_mods.protocol_scanners.https_scanner.https_scanner(
+                        address, port
+                    )
 
         print("Finished test for https_scanner failing when use a bad port value\n")
+
+    def test_05_patch(self):
+        """ Tests that the all exceptions are caught correctly"""
+        print("\nStarting test for https_scanner correctly catching exceptions")
+        test_list = [
+            (requests.exceptions.HTTPError, "HTTPError -- "),
+            (requests.exceptions.ConnectionError, "ConnectionError -- "),
+            (Exception, "OtherError -- "),
+        ]
+        for test_tuple in test_list:
+            test_exception, test_string = test_tuple
+            with patch(
+                "scan_mods.protocol_scanners.https_scanner.requests.Session.get",
+                side_effect=test_exception,
+            ):
+                result = scan_mods.protocol_scanners.https_scanner.https_scanner(
+                    "192.168.89.80",
+                    port=443,
+                )
+                self.assertIsNotNone(result)
+                self.assertGreaterEqual(len(result), 1)
+                self.assertIsInstance(result, dict)
+                for key in result.keys():
+                    self.assertIsInstance(key, str)
+                    self.assertEqual(key, "ERROR")
+                    self.assertEqual(result[key], test_string)
+        print("Finished test for https_scanner correctly catching exceptions")
+
+    def test_06_can_create_valid_json(self):
+        """
+        Tests that the HTTPS port scanner can create valid json
+        """
+        print("\nStarting test https_scanner can create valid JSON")
+        dict_of_results = {}
+        for address in self.good_servers:
+            dict_of_results[
+                address
+            ] = scan_mods.protocol_scanners.https_scanner.https_scanner(address)
+        json_output = json.dumps(dict_of_results)
+        self.assertIsNotNone(json_output)
+        self.assertGreaterEqual(len(json_output), 1)
+        self.assertIsInstance(json_output, str)
+        print("Finished test https_scanner can create valid JSON\n")
 
 
 if __name__ == "__main__":
